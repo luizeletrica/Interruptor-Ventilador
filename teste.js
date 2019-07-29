d5f09078dcbf80e8a8583e5c22bbec16cd754814
luizeletrica <46749636+luizeletrica@users.noreply.github.com>
2019-07-29 20:11:15 -0300

    Update teste.js

--- teste.js
+++ teste.js
@@ -11,26 +11,19 @@ function AlteraRele1(){
     }
 }
 
-
 function AlteraRele2(){
     var estado_2 = document.getElementById("estado_2").innerHTML;
-	if(estado_2 === "1"){
+    if(estado_2 === "0"){
+        document.getElementById("estado_2").innerHTML="1";
+        document.getElementById("VENT").innerHTML="<div class='VENT_ligado'></div>";
+		document.getElementById("botao_2").innerHTML="<a href='/?2_desligar' class='botao_2'>Desliga</a>";
+    } else {
         document.getElementById("estado_2").innerHTML="0";
         document.getElementById("VENT").innerHTML="<div class='VENT_desligado'></div>";
         document.getElementById("botao_2").innerHTML="<a href='/?2_ligar' class='botao_2'>Ventilador</a>";
-   
+    }
 }
  
- function AlteraRele4(){
-    var estado_4 = document.getElementById("estado_4").innerHTML;
-    if(estado_4 === "1"){
-        document.getElementById("estado_4").innerHTML="0";
-        document.getElementById("EXAUST").innerHTML="<div class='EXAUST_desligado'></div>";
-        document.getElementById("botao_4").innerHTML="<a href='/?4_ligar' class='botao_4'>Exaustor</a>";
-    } 
-}
-
-
  function AlteraRele3(){
     var estado_3 = document.getElementById("estado_3").innerHTML;
     if(estado_3 === "0"){
@@ -41,5 +34,18 @@ function AlteraRele2(){
         document.getElementById("estado_3").innerHTML="0";
         document.getElementById("DESLIG").innerHTML="<div class='DESLIG_desligado'></div>";
         document.getElementById("botao_3").innerHTML="<a href='/?3_ligar' class='botao_3'>Desligado</a>";
+    }
+}
+
+ function AlteraRele4(){
+    var estado_4 = document.getElementById("estado_4").innerHTML;
+    if(estado_4 === "0"){
+        document.getElementById("estado_4").innerHTML="1";
+        document.getElementById("EXAUST").innerHTML="<div class='EXAUST_ligado'></div>";
+		document.getElementById("botao_4").innerHTML="<a href='/?4_desligar' class='botao_4'>Desligar</a>";
+    } else {
+        document.getElementById("estado_4").innerHTML="0";
+        document.getElementById("EXAUST").innerHTML="<div class='EXAUST_desligado'></div>";
+        document.getElementById("botao_4").innerHTML="<a href='/?4_ligar' class='botao_4'>Exaustor</a>";
     }
 }
