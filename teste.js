0171b45b69c7790fc91fe405054642430ae62454
luizeletrica <46749636+luizeletrica@users.noreply.github.com>
2019-07-29 22:29:54 -0300

    Update teste.js

--- teste.js
+++ teste.js
@@ -33,23 +33,23 @@ function AlteraRele2(){
 	var estado_2 = document.getElementById("estado_2").innerHTML;
     var estado_3 = document.getElementById("estado_3").innerHTML;
     var estado_4 = document.getElementById("estado_4").innerHTML;
-    if(estado_3 === "0"){
-        document.getElementById("estado_3").innerHTML="1";
-        document.getElementById("DESLIG").innerHTML="<div class='DESLIG_ligado'></div>";
-		document.getElementById("botao_3").innerHTML="<a href='' class='botao_3'>DESLIGAR</a>";
-		
-		document.getElementById("estado_2").innerHTML="0";
-        document.getElementById("VENT").innerHTML="<div class='VENT_desligado'></div>";
-        document.getElementById("botao_2").innerHTML="<a href='/?2_desligar' class='botao_2'>Ventilador</a>";
-		
-		document.getElementById("estado_2").innerHTML="0";
-        document.getElementById("VENT").innerHTML="<div class='VENT_desligado'></div>";
-        document.getElementById("botao_2").innerHTML="<a href='/?2_desligar' class='botao_2'>Ventilador</a>";
-		
-    } else {
+    if(estado_3 === "1"){
         document.getElementById("estado_3").innerHTML="0";
         document.getElementById("DESLIG").innerHTML="<div class='DESLIG_desligado'></div>";
         document.getElementById("botao_3").innerHTML="<a href='' class='botao_3'>DESLIGADO</a>";
+		
+		document.getElementById("estado_2").innerHTML="0";
+        document.getElementById("VENT").innerHTML="<div class='VENT_desligado'></div>";
+        document.getElementById("botao_2").innerHTML="<a href='/?2_desligar' class='botao_2'>Ventilador</a>";
+		
+		document.getElementById("estado_4").innerHTML="0";
+        document.getElementById("EXAUST").innerHTML="<div class='EXAUST_desligado'></div>";
+        document.getElementById("botao_4").innerHTML="<a href='/?4_ligar' class='botao_4'>Exaustor</a>";
+		
+    } else {
+     	document.getElementById("estado_3").innerHTML="1";
+        document.getElementById("DESLIG").innerHTML="<div class='DESLIG_ligado'></div>";
+		document.getElementById("botao_3").innerHTML="<a href='' class='botao_3'>DESLIGAR</a>";
     }
 }
 
