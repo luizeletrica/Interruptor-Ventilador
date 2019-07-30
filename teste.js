fd52d2e5739d74bf768e775aa1778a6923065da5
luizeletrica <46749636+luizeletrica@users.noreply.github.com>
2019-07-29 22:13:19 -0300

    Update teste.js

--- teste.js
+++ teste.js
@@ -18,9 +18,10 @@ function AlteraRele2(){
         document.getElementById("estado_2").innerHTML="1";
         document.getElementById("VENT").innerHTML="<div class='VENT_ligado'></div>";
 		document.getElementById("botao_2").innerHTML="<a href='' class='botao_2'>Ligado</a>";
-		document.getElementById("estado_3").innerHTML="0";
-        document.getElementById("DESLIG").innerHTML="<div class='DESLIG_desligado'></div>";
-        document.getElementById("botao_3").innerHTML="<a href='/?3_ligar' class='botao_3'>DESLIGADO</a>";
+		
+		document.getElementById("estado_3").innerHTML="1";
+        document.getElementById("DESLIG").innerHTML="<div class='DESLIG_ligado'></div>";
+		document.getElementById("botao_3").innerHTML="<a href='' class='botao_3'>DESLIGAR</a>";
     } else {
         document.getElementById("estado_2").innerHTML="0";
         document.getElementById("VENT").innerHTML="<div class='VENT_desligado'></div>";
@@ -48,9 +49,10 @@ function AlteraRele2(){
         document.getElementById("estado_4").innerHTML="1";
         document.getElementById("EXAUST").innerHTML="<div class='EXAUST_ligado'></div>";
 		document.getElementById("botao_4").innerHTML="<a href='' class='botao_4'>Ligado</a>";
-		document.getElementById("estado_3").innerHTML="0";
-        document.getElementById("DESLIG").innerHTML="<div class='DESLIG_desligado'></div>";
-        document.getElementById("botao_3").innerHTML="<a href='/?3_ligar' class='botao_3'>DESLIGADO</a>";
+		
+		document.getElementById("estado_3").innerHTML="1";
+        document.getElementById("DESLIG").innerHTML="<div class='DESLIG_ligado'></div>";
+		document.getElementById("botao_3").innerHTML="<a href='' class='botao_3'>DESLIGAR</a>";
     } else {
         document.getElementById("estado_4").innerHTML="0";
         document.getElementById("EXAUST").innerHTML="<div class='EXAUST_desligado'></div>";
