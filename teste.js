80f29cd0b5cd18cfe78f81e404104170654ad934
luizeletrica <46749636+luizeletrica@users.noreply.github.com>
2019-07-30 10:24:33 -0300

    Update teste.js

--- teste.js
+++ teste.js
@@ -40,10 +40,7 @@ function AlteraRele1(){
         document.getElementById("DESLIG").innerHTML="<div class='DESLIG_desligado'></div>";
         document.getElementById("botao_3").innerHTML="<a href='/?3_desligar' class='botao_3'>DESLIGADO</a>";
 		
-		document.getElementById("estado_4").innerHTML="0";
-        document.getElementById("EXAUST").innerHTML="<div class='EXAUST_desligado'></div>";
-        document.getElementById("botao_4").innerHTML="<a href='/?4_ligar' class='botao_4'>Exaustor</a>";
-		
+				
     }
 }
 
@@ -55,13 +52,10 @@ function AlteraRele1(){
         document.getElementById("EXAUST").innerHTML="<div class='EXAUST_ligado'></div>";
 		document.getElementById("botao_4").innerHTML="<a href='' class='botao_4'>Ligado</a>";
 		
-		document.getElementById("estado_3").innerHTML="1";
-        document.getElementById("DESLIG").innerHTML="<div class='DESLIG_ligado'></div>";
-		document.getElementById("botao_3").innerHTML="<a href='' class='botao_3'>DESLIGAR</a>";
+	
     } else {
         document.getElementById("estado_4").innerHTML="0";
         document.getElementById("EXAUST").innerHTML="<div class='EXAUST_desligado'></div>";
         document.getElementById("botao_4").innerHTML="<a href='/?4_ligar' class='botao_4'>Exaustor</a>";
     }
 }
- 
