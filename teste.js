069f8ad32d3f52a98b27446fba9fdd512650b527
luizeletrica <46749636+luizeletrica@users.noreply.github.com>
2019-07-31 09:25:17 -0300

    Update teste.js

--- teste.js
+++ teste.js
@@ -56,10 +56,7 @@ function AlteraRele1(){
         document.getElementById("DESLIG").innerHTML="<div class='DESLIG_desligado'></div>";
         document.getElementById("botao_3").innerHTML="<a href='' class='botao_3'>DESLIGADO</a>";
 		
-		
-		
-				
-    }
+	 }
 }
 
  function AlteraRele4(){
