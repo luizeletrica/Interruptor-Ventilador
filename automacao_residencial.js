6c9fbdbdd38042b710027d053ecd1d3c9141a06c
luizeletrica <46749636+luizeletrica@users.noreply.github.com>
2020-05-21 16:09:09 -0300

    Update automacao_residencial.js

--- automacao_residencial.js
+++ automacao_residencial.js
@@ -21,10 +21,11 @@ function AlteraRele2(){
     } else {
        document.getElementById("estado_2").innerHTML="1";
         document.getElementById("VENT").innerHTML="<div class='VENT_ligado'></div>";
-		document.getElementById("botao_2").innerHTML="ligado";
+		document.getElementById("botao_2").innerHTML="<a href='/?2_ligar' class='botao_2'>Ligado</a>";
 		document.getElementById("estado_3").innerHTML="1";
         document.getElementById("DESLIG").innerHTML="<div class='DESLIG_ligado'></div>";
-		document.getElementById("botao_3").innerHTML="<a href='/?4_desligar' class='botao_3'>Desligar</a>";
+		document.getElementById("botao_3").innerHTML="<a href='/?2_desligar' class='botao_3'>Desligar</a>";
+	    document.getElementById("botao_4").innerHTML="<a href='/?2_desligar' class='botao_4'>Desligar</a>";
     }
 }
  
@@ -33,10 +34,11 @@ function AlteraRele2(){
     if(estado_4 === "0"){
         document.getElementById("estado_4").innerHTML="1";
         document.getElementById("EXAUST").innerHTML="<div class='EXAUST_ligado'></div>";
-		document.getElementById("botao_4").innerHTML="ligado";
+		document.getElementById("botao_4").innerHTML="<a href='/?4_ligar' class='botao_4'>Ligado</a>";
 		document.getElementById("estado_3").innerHTML="1";
         document.getElementById("DESLIG").innerHTML="<div class='DESLIG_ligado'></div>";
 		document.getElementById("botao_3").innerHTML="<a href='/?4_desligar' class='botao_3'>Desligar</a>";
+	    document.getElementById("botao_2").innerHTML="<a href='/?4_desligar' class='botao_2'>Desligar</a>";
     } else {
         document.getElementById("estado_4").innerHTML="0";
         document.getElementById("EXAUST").innerHTML="<div class='EXAUST_desligado'></div>";
