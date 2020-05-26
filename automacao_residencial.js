5f03567db976b2c852014b533ab1954217bc5e58
luizeletrica <46749636+luizeletrica@users.noreply.github.com>
2020-05-25 21:02:13 -0300

    Update automacao_residencial.js

--- automacao_residencial.js
+++ automacao_residencial.js
@@ -12,24 +12,6 @@ function AlteraRele1(){
 }
 
 
-function AlteraRele2(){
-    var estado_2 = document.getElementById("estado_2").innerHTML;
-	if(estado_2 === "0"){
-        document.getElementById("estado_2").innerHTML="1";
-        document.getElementById("VENT").innerHTML="<div class='VENT_ligado'></div>";
-		document.getElementById("botao_2").innerHTML="<a href='/?2_ligar' class='botao_2'>Ligado</a>";
-		document.getElementById("estado_3").innerHTML="1";
-        document.getElementById("DESLIG").innerHTML="<div class='DESLIG_ligado'></div>";
-		document.getElementById("botao_3").innerHTML="<a href='/?2_desligar' class='botao_3'>Desligar</a>";
-	    document.getElementById("botao_4").innerHTML="<a href='/?2_desligar' class='botao_4'>Desligar</a>";
-       
-	} else {
-       
-document.getElementById("estado_2").innerHTML="0";
-        document.getElementById("VENT").innerHTML="<div class='VENT_desligado'></div>";
-        document.getElementById("botao_2").innerHTML="<a href='/?2_ligar' class='botao_2'>Ventilador</a>";
-    }
-}
  
  function AlteraRele4(){
     var estado_4 = document.getElementById("estado_4").innerHTML;
@@ -48,6 +30,25 @@ document.getElementById("estado_2").innerHTML="0";
     }
 }
 
+
+function AlteraRele2(){
+    var estado_2 = document.getElementById("estado_2").innerHTML;
+	if(estado_2 === "0"){
+        document.getElementById("estado_2").innerHTML="1";
+        document.getElementById("VENT").innerHTML="<div class='VENT_ligado'></div>";
+		document.getElementById("botao_2").innerHTML="<a href='/?2_ligar' class='botao_2'>Ligado</a>";
+		document.getElementById("estado_3").innerHTML="1";
+        document.getElementById("DESLIG").innerHTML="<div class='DESLIG_ligado'></div>";
+		document.getElementById("botao_3").innerHTML="<a href='/?2_desligar' class='botao_3'>Desligar</a>";
+	    document.getElementById("botao_4").innerHTML="<a href='/?2_desligar' class='botao_4'>Desligar</a>";
+       
+	} else {
+       
+document.getElementById("estado_2").innerHTML="0";
+        document.getElementById("VENT").innerHTML="<div class='VENT_desligado'></div>";
+        document.getElementById("botao_2").innerHTML="<a href='/?2_ligar' class='botao_2'>Ventilador</a>";
+    }
+}
 
  function AlteraRele3(){
     var estado_3 = document.getElementById("estado_3").innerHTML;
