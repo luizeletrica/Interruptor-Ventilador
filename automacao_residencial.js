730d9dd83bbaba555db1f53016726079099d9ec0
luizeletrica <46749636+luizeletrica@users.noreply.github.com>
2020-05-26 01:07:20 -0300

    Update automacao_residencial.js

--- automacao_residencial.js
+++ automacao_residencial.js
@@ -48,10 +48,8 @@ function AlteraRele1(){
 		document.getElementById("estado_3").innerHTML="1";
         document.getElementById("DESLIG").innerHTML="<div class='DESLIG_ligado'></div>";
 		document.getElementById("botao_3").innerHTML="<a href='/?3_desligar' class='botao_3'>DESLIGAR</a>";
-		
-		
-    } else {
-     	
+				
+    } else {     	
 		
 		document.getElementById("estado_3").innerHTML="0";
         document.getElementById("DESLIG").innerHTML="<div class='DESLIG_desligado'></div>";
